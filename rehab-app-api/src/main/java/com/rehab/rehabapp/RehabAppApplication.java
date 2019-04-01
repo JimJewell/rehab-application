cf752cbbbad8f51875a3d67077d074c04aec0309
JimJewell <jimhjewell@gmail.com>
2019-04-01 10:46:37 -0400

    first commit

---
+++ rehab-app-api/src/main/java/com/rehab/rehabapp/RehabAppApplication.java
@@ -0,0 +1,13 @@
+package com.rehab.rehabapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RehabAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RehabAppApplication.class, args);
+	}
+
+}
