efe4ed4e806d3448621974bc723bc5e833e4c022
Parker-Bartlett <prbartlett1@gmail.com>
2019-04-04 10:04:07 -0400

    adding back end

--- rehab-app-api/src/main/java/com/rehab/rehabapp/controllers/SurveyController.java
+++ rehab-app-api/src/main/java/com/rehab/rehabapp/controllers/SurveyController.java
@@ -4,6 +4,7 @@ import java.util.Collection;
 
 import javax.annotation.Resource;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.rehab.rehabapp.models.Question;
 import com.rehab.rehabapp.models.Survey;
 import com.rehab.rehabapp.repositories.QuestionRepository;
 import com.rehab.rehabapp.repositories.SurveyRepository;
@@ -52,4 +54,31 @@ public class SurveyController {
 		Long surveyId = surveyRepo.findByName(json.getString("surveyName")).getId();
 		return surveyId;
 	}
+	
+	@PostMapping("/submit")
+	public void submitSurvey(@RequestBody String body) throws JSONException {
+		JSONObject json = new JSONObject(body);
+		String name = json.getString("name");
+		Survey survey = surveyRepo.findByName(name);
+		JSONArray questionCollections = json.getJSONArray("questions");
+		
+		JSONObject jsonOne;
+		
+		for (int i = 0 ; i < questionCollections.length() ; i++ ) {
+			 jsonOne = (JSONObject) questionCollections.get(i);
+			 String nameToMake = jsonOne.getString("name");
+			 String value = jsonOne.getString("value");
+			 questionRepo.save(new Question(nameToMake, value, survey));
+		}
+	}
 }
+
+
+
+
+
+
+
+
+
+
