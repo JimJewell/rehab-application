e084733660631c1a0c28164cf500439e59c81e44
mateofrito <46681151+mateofrito@users.noreply.github.com>
2019-04-05 13:38:56 -0400

    added more questions

--- rehab-app-api/src/main/java/com/rehab/rehabapp/Initializer.java
+++ rehab-app-api/src/main/java/com/rehab/rehabapp/Initializer.java
@@ -24,6 +24,8 @@ public class Initializer implements CommandLineRunner {
 		Survey surveyLower = new Survey("Lower Extremity Assessment");
 		Survey surveyPhysical = new Survey("Physical Function Assessment");
 		Survey surveyNeck = new Survey("Neck Assessment");
+		Survey survey3 = new Survey("Upper Extremity Assessment");
+        Survey survey4 = surveyRepo.save(new Survey("Low Back Pain Assessment"));
 		
 		Question lowerQuestion1 = new Question("Your usual work, housework, or school activites?", surveyLower);
 		Question lowerQuestion2 = new Question("Your usual hobbies, sports, or recreational activities?", surveyLower);
@@ -66,9 +68,37 @@ public class Initializer implements CommandLineRunner {
 		Question physcialQuestion7 = new Question("Lifting or carrying groceries?", surveyPhysical);
 		Question physcialQuestion8 = new Question("Heavy work around the house like scrubbing floors, or lifting or moving heavy furniture?", surveyPhysical);
 		
+		// Upper Extremity Assessment //
+        Question question11 = new Question("Open a tight or new jar", survey3);
+        Question question12 = new Question("Do heavy household chores (e.g., wash walls, floors)", survey3);
+        Question question13 = new Question("Carry a shopping bag or briefcase", survey3);
+        Question question14 = new Question("Wash your back", survey3);
+        Question question15 = new Question("Use a knife to cut food", survey3);
+        Question question16 = new Question("Recreational activities in which you take force or impact through your arm, shoulder or hand(e.g., golf, hammering, tennis.etc)", survey3);
+        Question question17 = new Question("Extend your arm, shoulder or hand problem interfered with your nomral social activities with family, friends, neighbors, etc?", survey3);
+        Question question18 = new Question("Limitations in your work or other regular daily activities as a resultof your arm, shoulder or hand problem?", survey3);
+        Question question19 = new Question("Severity of arm, shoudler or hand pain?", survey3);
+        Question question20 = new Question("Presence of tingling (pins and needles) in your arm, shoulder or hand", survey3);
+        Question question21 = new Question("Difficulty you've had sleeping because of your pain in your arm, shoulder or hand?", survey3);
+        Question question22 = new Question("Using your usual technique for your work", survey3);
+        Question question23 = new Question("Doing your usual work because of arm, shoulder or hand pain", survey3);
+        Question question24 = new Question("Spending your usual amount of time doing your work", survey3);
+        Question question25 = new Question("Using your usual technique for playing", survey3);
+        Question question26 = new Question("Playing because of arm, shoulder or hand pain", survey3);
+        Question question27 = new Question("Playing as well as you would like", survey3);
+        Question question28 = new Question("Spending your usual amount of time practicing or playing", survey3);
+        
+        // Low Back Pain Assessment //
+        Question question29 = questionRepo.save(new Question("Pain Intensity", survey4));
+        Question question30 = questionRepo.save(new Question("Washing and Dressing", survey4));
+        Question question31 = questionRepo.save(new Question("Lifting", survey4));
+        Question question32 = questionRepo.save(new Question("Walking", survey4));
+		
 		surveyRepo.save(surveyLower);
 		surveyRepo.save(surveyNeck);
 		surveyRepo.save(surveyPhysical);
+		surveyRepo.save(survey3);
+		
 		
 		questionRepo.save(lowerQuestion1); 
 		questionRepo.save(lowerQuestion2);
@@ -110,6 +140,25 @@ public class Initializer implements CommandLineRunner {
 		questionRepo.save(physcialQuestion6); 
 		questionRepo.save(physcialQuestion7);
 		questionRepo.save(physcialQuestion8);
+		
+		questionRepo.save(question11);
+        questionRepo.save(question12);
+        questionRepo.save(question13);
+        questionRepo.save(question14);
+        questionRepo.save(question15);
+        questionRepo.save(question16);
+        questionRepo.save(question17);
+        questionRepo.save(question18);
+        questionRepo.save(question19);
+        questionRepo.save(question20);
+        questionRepo.save(question21);
+        questionRepo.save(question22);
+        questionRepo.save(question23);
+        questionRepo.save(question24);
+        questionRepo.save(question25);
+        questionRepo.save(question26);
+        questionRepo.save(question27);
+        questionRepo.save(question28);
 		
 		System.out.println("Delete this");
 		System.out.println("Delete this too");
