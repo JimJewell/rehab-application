1d8706c17bf894f91a2378701b6de4d3364e2db1
mateofrito <46681151+mateofrito@users.noreply.github.com>
2019-04-05 09:42:27 -0400

    corrected some stuff on the header and the video

--- rehab-app-front-end-user/src/components/Video.js
+++ rehab-app-front-end-user/src/components/Video.js
@@ -1,17 +1,14 @@
-import React, { Component } from 'react'
-import '../App.css'
+import React, { Component } from "react";
+import "../App.css";
 
-export default class Video extends Component { 
-
+export default class Video extends Component {
   render() {
     return (
-        <div>
-            <div className='flex-row-right'>
-                <h1>(     </h1><h1 className='cheeks'>)  )</h1>
-                <h1 className='hero'>-</h1> 
-                <h1 className='bang'>  c===3 </h1>
-            </div>
+      <div>
+        <div className="flex-row-right">
+          <h1>This is the video page.</h1>
         </div>
-    )
+      </div>
+    );
   }
 }
