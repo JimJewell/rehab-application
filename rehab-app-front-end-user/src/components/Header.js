3d5b4500822a2fab96b4142125ab445a4b4464b8
mateofrito <46681151+mateofrito@users.noreply.github.com>
2019-04-10 13:41:14 -0400

    navbar is driving me bonkers

--- rehab-app-front-end-user/src/components/Header.js
+++ rehab-app-front-end-user/src/components/Header.js
@@ -4,23 +4,30 @@ export default ({ updateCurrentLocation, setUserType, userType }) => {
   return (
     <div>
       <header className="header">
-        <h1 className="logo">RehabIT</h1>
         <nav className="nav">
+          <img
+            className="logo"
+            alt={"logo"}
+            src={"./icons/rehabiticontransparent"}
+          />
           <ul className="nav-list">
             {userType === "none" && (
-              <div>
-                <li className="nav-list-item">
-                  <a onClick={() => setUserType("patient")}>Patients</a>
-                </li>
-                <li className="nav-list-item">
-                  <a onClick={() => setUserType("professionals")}>
-                    Medical Professionals
-                  </a>
-                </li>
+              <div className="dropdown">
+                <button className="dropbtn">Login</button>
+                <div className="dropdown-content">
+                  <li className="nav-list-item">
+                    <a onClick={() => setUserType("patient")}>Patients</a>
+                  </li>
+                  <li className="nav-list-item">
+                    <a onClick={() => setUserType("professionals")}>
+                      Medical Professionals
+                    </a>
+                  </li>
+                </div>
               </div>
             )}
             {userType === "patient" && (
-              <div>
+              <div className="dropDown">
                 <li className="nav-list-item">
                   <a onClick={() => updateCurrentLocation("survey")}>Survey</a>
                 </li>
@@ -30,7 +37,7 @@ export default ({ updateCurrentLocation, setUserType, userType }) => {
               </div>
             )}
             {userType === "professional" && (
-              <div>
+              <div className="dropDown">
                 <li className="nav-list-item">
                   <a onClick={() => updateCurrentLocation("survey")}>Reports</a>
                 </li>
--- rehab-app-front-end-user/src/components/home.js
+++ rehab-app-front-end-user/src/components/home.js
@@ -5,19 +5,7 @@ export default class Home extends Component {
   render() {
     return (
       <div>
-        <div className="splashBacker">
-          <div className="login">
-            <p>Medical Professionals</p>
-            <button className="genericButton">Sign-In</button>
-            <p>Patients</p>
-            <button
-              className="genericButton"
-              onClick={() => this.patientLogin()}
-            >
-              Sign-In
-            </button>
-          </div>
-        </div>
+        <p>Rehabit.com</p>
       </div>
     );
   }
