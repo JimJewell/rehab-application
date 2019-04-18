8805329e0911f4b076e8cd4b26628dcc14393244
mateofrito <46681151+mateofrito@users.noreply.github.com>
2019-04-18 10:56:42 -0400

    login is working and fixed vr icon

---
+++ rehab-app-front-end-user/src/components/Modal.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+
+import "./modal.css";
+class Modal extends Component {
+  render() {
+    return (
+      <div className="modal">
+        <h1> this is a login dealio</h1>
+      </div>
+    );
+  }
+}
+
+export default Modal;
