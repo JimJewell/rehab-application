0ee1733cb910a11d2905e0afcbef022845325499
Nick Miner <nick.miner18@gmail.com>
2019-04-22 10:57:55 -0500

    working onclick events

--- rehab-app-front-end-user/src/components/Scene.js
+++ rehab-app-front-end-user/src/components/Scene.js
@@ -1,53 +1,88 @@
 import 'aframe'
 import React, {Component} from 'react'
 
+  // AFRAME.registerComponent("foo", {
+  // init: function() {
+  // this.el.addEventListener("click", (e)=>{
+  //  console.log('click')
+  // })
+  // }
+  // })
+  
+
 class Scene extends Component {
-
-  AFRAME.registerComponent('cursor-click1', {
-    init: function(){
-      let skeleton1 = document.querySelector("skeleton1")
-      skeleton1.addEventListener('click', ()=> {
-        document.querySelector("skeleton__text1").toggleAttribute("visible__text")
-      }) 
-    }
-  })
-
-  AFRAME.skeleton1.addEventListener('click',
-  function(){
-    console.log('click')
-  }
-
   render () {
     return (
-      <div className="a-scene">
-        <a-scene>
+      <div id="a-scene" className="a-scene">
+        <a-scene cursor="rayOrigin: mouse">
 
-        <a-entity windows-motion-controls="hand: left"></a-entity>
-        <a-entity windows-motion-controls="hand: right"></a-entity>
 
-        {/* <a-entity>
+        {/* <a-camera position="0 1 0">
+          <a-cursor scale="0.5 0.5" color="black"></a-cursor>
+        </a-camera> */}
+
+        <a-entity raycaster="objects: [data-raycastable]" cursor></a-entity>
+
+
+        {/* <a-entity windows-motion-controls="hand: left"></a-entity>
+        <a-entity windows-motion-controls="hand: right"></a-entity> */}
+
+        <a-entity>
         <a-gltf-model 
+        data-raycastable
         src={require('./images/skeleton.gltf')}
         position="-2 0 -3"
-        scale=".25 .25 .25">
-        </a-gltf-model>
-        </a-entity> */}
+        scale=".25 .25 .25"
+        onclick="{
+          let textBox1 = document.querySelector('#skeleton__text1')
+          if(textBox1.getAttribute('position').y == -10){
+            textBox1.setAttribute('position', {x:2,  y:3,  z:-4})
 
-        <a-entity className="skeleton1">
-        <a-gltf-model 
-        src={require('./images/skeleton.gltf')}
-        position="0 0 -3"
-        scale=".25 .25 .25">
+          } else {
+            textBox1.setAttribute('position', {x:2,  y:-10,  z:-4})
+
+          }
+      }">
         </a-gltf-model>
         </a-entity>
 
-        {/* <a-entity>
+        <a-entity data-raycastable>
+        <a-gltf-model
+        data-raycastable
+        src={require('./images/skeleton.gltf')}
+        position="0 0 -3"
+        scale=".25 .25 .25"
+        onclick="{
+          let textBox1 = document.querySelector('#skeleton__text2')
+          if(textBox1.getAttribute('position').y == -10){
+            textBox1.setAttribute('position', {x:4,  y:4,  z:-4})
+
+          } else {
+            textBox1.setAttribute('position', {x:4,  y:-10,  z:-4})
+
+          }
+      }">
+
+        </a-gltf-model>
+        </a-entity>
+
+        <a-entity>
         <a-gltf-model 
         src={require('./images/skeleton.gltf')}
         position="2 0 -3"
-        scale=".25 .25 .25">
+        scale=".25 .25 .25"
+        onclick="{
+          let textBox1 = document.querySelector('#skeleton__text3')
+          if(textBox1.getAttribute('position').y == -10){
+            textBox1.setAttribute('position', {x:6,  y:3,  z:-4})
+
+          } else {
+            textBox1.setAttribute('position', {x:6,  y:-10,  z:-4})
+
+          }
+      }">
         </a-gltf-model>
-        </a-entity> */}
+        </a-entity>
 
         <a-plane 
           position="0 0 0" 
@@ -57,11 +92,24 @@ class Scene extends Component {
           color="grey">
           </a-plane>
 
-        <a-entity className="skeleton__text1"
+        <a-entity id="skeleton__text1"
         text="value: Hello World"
-        position="1 1 -4"
+        position="2 -10 -4"
         scale="10 10 1"
         ></a-entity>
+
+        <a-entity id="skeleton__text2"
+        text="value: Hello World"
+        position="4 -10 -4"
+        scale="10 10 1"
+        ></a-entity>
+
+        <a-entity id="skeleton__text3"
+        text="value: Hello World"
+        position="6 -10 -4"
+        scale="10 10 1"
+        ></a-entity>
+        
       </a-scene>
       </div>  
     );
